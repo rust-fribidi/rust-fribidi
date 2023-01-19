4c9be8f7a88f603c685216dc2873d145f7ce1722
Baron Leonardo <mohamed.a.elmeligy@protonmail.com>
2023-01-19 07:15:47 +0200

    implement function get_par_direction and test it

--- src/fribidi.rs
+++ src/fribidi.rs
@@ -12,7 +12,7 @@ pub enum ParagraphType
 {
     LeftToRight     = fribidi_bindings::FriBidiParType_FRIBIDI_PAR_LTR,
     RightToLeft     = fribidi_bindings::FriBidiParType_FRIBIDI_PAR_RTL,
-    OtherNetural    = fribidi_bindings::FriBidiParType_FRIBIDI_PAR_ON,
+    OtherNeutral    = fribidi_bindings::FriBidiParType_FRIBIDI_PAR_ON,
     WeakLeftToRight = fribidi_bindings::FriBidiParType_FRIBIDI_PAR_WLTR,
     WeakRightToLeft = fribidi_bindings::FriBidiParType_FRIBIDI_PAR_WRTL,
 }
@@ -205,6 +205,36 @@ impl Fribidi
 
         format!("{:?}", char_type)
     }
+
+    /// fribidi_get_par_direction - get base paragraph direction
+    ///
+    /// This function finds the base direction of a single paragraph,
+    /// as defined by rule P2 of the Unicode Bidirectional Algorithm available at
+    /// http://www.unicode.org/reports/tr9/#P2.
+    ///
+    /// You typically do not need this function as
+    /// fribidi_get_par_embedding_levels() knows how to compute base direction
+    /// itself, but you may need this to implement a more sophisticated paragraph
+    /// direction handling.  Note that you can pass more than a paragraph to this
+    /// function and the direction of the first non-neutral paragraph is returned,
+    /// which is a very good heuristic to set direction of the neutral paragraphs
+    /// at the beginning of text.  For other neutral paragraphs, you better use the
+    /// direction of the previous paragraph.
+    ///
+    /// Returns: Base pargraph direction.  No weak paragraph direction is returned,
+    /// only LeftToRight, RightToLeft, or OtherNeutral.
+    ///
+    pub fn get_par_direction (char_types: &Vec<CharType>) -> ParagraphType
+    {
+        let par_direction = unsafe {
+            // let bidi_type_name = fribidi_bindings::fribidi_get_bidi_type_name(char_type as u32);
+            // fribidi_bindings::fribidi_get_par_direction(bidi_type_name, len);
+            fribidi_bindings::fribidi_get_par_direction(char_types.as_ptr() as *const u32, char_types.len() as i32)
+        };
+
+        unsafe { std::mem::transmute (par_direction) }
+
+    }
 }
 
 #[cfg(test)]
@@ -296,5 +326,16 @@ mod test
 
         let char_type_name = Fribidi::get_bidi_type_name(char_type);
         assert_eq!(char_type_name, gt);
+    }
+
+    #[test]
+    fn test_get_par_direction ()
+    {
+        let char_types = vec![CharType::ArabicLetter, CharType::ArabicNumeral, CharType::LeftToRight, CharType::LeftToRight];
+        let gt = ParagraphType::RightToLeft;
+
+        let par_dir = Fribidi::get_par_direction(&char_types);
+
+        assert_eq!(par_dir, gt);
     }
 }
